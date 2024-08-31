650ec0e254ad34185a1283e5c4cd161fd906dd27
M1oGh <96004731+M1oGh@users.noreply.github.com>
2024-08-31 16:56:58 +0200

    Update __main__.py

--- climate_risk_calc/__main__.py
+++ climate_risk_calc/__main__.py
@@ -2,5 +2,4 @@ import climate_risk_calc.tools.graph_designer
 from climate_risk_calc import controller
 
 if __name__ == "__main__":
-    climate_risk_calc.tools.graph_designer.hi()
     controller.start_view()
